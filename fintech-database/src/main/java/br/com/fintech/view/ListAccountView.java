43356eadedc1c0bc30efc53b1c5b88348bcef318
giseli de Andrade <giseliandradecosta@gmail.com>
2024-10-13 20:42:03 -0300

    adição de estrurura do banco

---
+++ fintech-database/src/main/java/br/com/fintech/view/ListAccountView.java
@@ -0,0 +1,21 @@
+package br.com.fintech.view;
+
+import br.com.fintech.model.Account;
+import br.com.fintech.dao.AccountDao;
+import java.sql.SQLException;
+import java.util.List;
+
+public class ListAccountView {
+    public static void main(String[] args) {
+        try {
+            AccountDao dao = new AccountDao();
+            List<Account> accounts = dao.getAll();
+            for (Account account : accounts) {
+                System.out.println(account.getId() + " - " + account.getBalance() + " - " + account.getNumberAccount()+ " - " + account.getNumberBank() + " - " + account.getCreatedAt() + " - " + account.getUpdateAt()+ " - " + account.getAgency());
+            }
+            dao.closeConnection();
+        } catch (SQLException e) {
+            System.err.println(e.getMessage());
+        }
+    }
+}
